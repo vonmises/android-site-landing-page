e7f665b8c1f4a725dc811a2a804bcb555fad6abc
vonmises <vonmises@users.noreply.github.com>
2015-10-26 12:25:46 +0300

    added scripts to be used on the page

---
+++ scripts/main.js
@@ -0,0 +1,13 @@
+function writeToFile(){
+    if (window.XMLHttpRequest)
+    {// code for IE7+, Firefox, Chrome, Opera, Safari
+        xmlhttp=new XMLHttpRequest();
+    }
+    else
+    {// code for IE6, IE5
+        xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    xmlhttp.open("GET","YOUR_FILE.txt",false);
+    xmlhttp.send();
+    xmlDoc=xmlhttp.responseText;
+}
